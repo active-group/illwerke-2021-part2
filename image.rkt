cfa76db790c085429165bb28ed9e7c8874114345
Mike Sperber <sperber@deinprogramm.de>
2021-09-27 09:37:20 +0200

    Update. && git push

--- image.rkt
+++ image.rkt
@@ -3,5 +3,7 @@
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname image) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
 ; Language -> Add Teachpack -> image.rkt -> OK
 ; dann Run / Start oben rechts
-(define x (+ 23 12))
+(define x
+  (+ 23 (* 12 14)))
+
 
