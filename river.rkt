acc3596ea2f2de789de9743c7dc3f3a58b100c83
Mike Sperber <sperber@deinprogramm.de>
2021-09-27 13:43:05 +0200

    Update. && git push

--- river.rkt
+++ river.rkt
@@ -1,3 +1,23 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname river) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Ein Fluss ist einer der folgenden:
+; - kommt aus einer Quelle - ODER -
+; - fließt aus zwei Flüssen zusammen: Hauptfluss und Nebenfluss
+;                                          ^^^^^          ^^^^^
+;                                          Selbstreferenz
+
+; Ein Fluss ist einer der folgenden:
+; - ein Bach aus einer Quelle - ODER -
+; - ein Zusammentreffen von Hauptfluss und Nebenfluss
+(define river
+  (signature (mixed creek confluence)))
+
+; Ein Bach hat folgende Eigenschaften:
+; - Ursprungsort
+(define-record creek
+  make-creek
+  creek?
+  (creek-origin string))
+
+
