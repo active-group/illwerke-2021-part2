1856423ace398d5622c01aa217b5b72641a0b164
Mike Sperber <sperber@deinprogramm.de>
2021-09-27 10:23:04 +0200

    Update. && git push

--- animals.rkt
+++ animals.rkt
@@ -1,3 +1,9 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname animals) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Datenanalyse
+
+; Ein Haustier:
+; - Katze
+; - Hund
+; - Schlange
