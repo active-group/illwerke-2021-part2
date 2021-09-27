a4543fa3ab8f6fbf8779927816a3f61838135d30
Mike Sperber <sperber@deinprogramm.de>
2021-09-27 09:49:06 +0200

    Update. && git push

--- image.rkt
+++ image.rkt
@@ -15,3 +15,24 @@
 
 (define overlay1 (overlay star1 circle1))
 
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+(above
+ (beside square1 circle1)
+ (beside circle1 square1))
+
+; Abstraktion
+; - kopieren
+; - Variablen einführen
+; - Variablen -> lambda
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
+
+
+
+
