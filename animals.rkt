d8716441a5cddb1d8153c3fdd8893ceb256a607e
Mike Sperber <sperber@deinprogramm.de>
2021-09-27 10:34:44 +0200

    Update. && git push

--- animals.rkt
+++ animals.rkt
@@ -3,7 +3,38 @@
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname animals) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Datenanalyse
 
-; Ein Haustier:
-; - Katze
-; - Hund
+; Datendefinition -> Code
+
+; Datendefinition
+; Ein Haustier ist eins der folgenden:  <-- Formulierung
+; - Katze - ODER -
+; - Hund - ODER -
 ; - Schlange
+; Fallunterscheidung
+; hier speziell: Aufzählung
+; "cat" "dog" "snake"
+(define pet
+  (signature (enum "cat" "dog" "snake")))
+
+; Ist Haustier niedlich?
+; C#: isCute
+(: cute? (pet -> boolean))
+
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #f)
+
+; Gerüst
+#;(define cute?
+  (lambda (pet)
+    ...))
+
+; Schablone
+(define cute?
+  (lambda (pet)
+    ; pet ist eine Fallunterscheidung
+    ; -> Verzweigung
+    ; Zweig: (<Bedingung> <Antwort>)
+    (cond
+      ((string=? pet "cat") ...)
+      ((string=? pet "dog") ...)
+      ((string=? pet "snake") ...))))
