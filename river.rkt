cf67c2d7276ecfb1991652e4b1715aa7b1ba6a8b
Mike Sperber <sperber@deinprogramm.de>
2021-09-27 13:59:06 +0200

    Update.

--- river.rkt
+++ river.rkt
@@ -29,3 +29,26 @@
   (confluence-location string)
   (confluence-main-stem river) ; Selbstbezug
   (confluence-tributary river)) ; Selbstbezug
+
+(define neckar1 (make-confluence "Rottweil" eschach prim))
+(define schlichem (make-creek "Tieringen"))
+(define neckar2 (make-confluence "Epfendorf" neckar1 schlichem))
+
+; Fließt Wasser von einem bestimmten Ort in Fluss?
+(: flows-from? (string river -> boolean))
+
+(check-expect (flows-from? "Heimliswald" eschach) #t)
+(check-expect (flows-from? "Heimliswald" neckar2) #t)
+(check-expect (flows-from? "Heimliswald" prim) #f)
+(check-expect (flows-from? "Berlin" neckar2) #f)
+
+(define flows-from?
+  (lambda (location river)
+    (match river
+      ((make-creek origin)
+       (string=? origin location))
+      ((make-confluence clocation main-stem tributary)
+       (or (string=? clocation location)
+           (flows-from? location main-stem) ; Selbstbezug
+           (flows-from? location tributary))))))
+
