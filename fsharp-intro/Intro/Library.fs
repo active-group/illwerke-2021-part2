b75a54055137d4492d1f9bd6fb4a707654155888
Mike Sperber <sperber@deinprogramm.de>
2021-09-28 09:42:38 +0200

    Update

--- fsharp-intro/Intro/Library.fs
+++ fsharp-intro/Intro/Library.fs
@@ -16,14 +16,18 @@ module Code =
   type Time = { Hour: int 
                 Minute: int }
 
-  type PreciseTime = { Hour: int; Minute: int; Seconds: int }
+  // Mehrdeutigkeit bzgl. Hour und Minute
+  // type PreciseTime = { Hour: int; Minute: int; Seconds: int }
 
   let time1 = { Hour = 12; Minute = 24 }
   let time2 = { Hour = 5; Minute = 37}
 
   // Minuten seit Mitternacht
-  let msm time: int =
+  let msm (time: Time): int =
     time.Hour * 60 + time.Minute
+
+  // Variante
+  let msm' ({ Hour = h; })
 
 module Say =
     let hello name =
