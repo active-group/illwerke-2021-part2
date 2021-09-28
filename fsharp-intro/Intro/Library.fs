0a58dbaf3e5fbd34a349aa89bda28d48cdc107d4
Mike Sperber <sperber@deinprogramm.de>
2021-09-28 09:33:12 +0200

    Update

--- fsharp-intro/Intro/Library.fs
+++ fsharp-intro/Intro/Library.fs
@@ -1,6 +1,7 @@
 ﻿namespace Intro
 
 module Code =
+  // Aufzählung
   type Pet = Dog | Cat | Snake
 
   // Ist Haustier niedlich?
@@ -10,6 +11,11 @@ module Code =
     | Cat -> true
     | Snake -> false 
 
+  // zusammengesetzte Daten
+  // type Time = { Hour: int; Minute: int }
+  type Time = { Hour: int 
+                Minute: int }
+                
 module Say =
     let hello name =
         printfn "Hello %s" name
