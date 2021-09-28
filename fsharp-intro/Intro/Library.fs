2b0258fb92938d31357be93b27a8dd5f13f25041
Mike Sperber <sperber@deinprogramm.de>
2021-09-28 09:25:31 +0200

    Update

--- fsharp-intro/Intro/Library.fs
+++ fsharp-intro/Intro/Library.fs
@@ -3,6 +3,10 @@
 module Code =
   type Pet = Dog | Cat | Snake
 
+  // Ist Haustier niedlich?
+  let isCute (pet: Pet): bool =
+    
+
 module Say =
     let hello name =
         printfn "Hello %s" name
