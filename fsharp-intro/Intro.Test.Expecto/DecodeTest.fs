9798ed7ba9fdeeb294f8c65f884b2a28d958acb9
Johannes Maier <johannes.maier@active-group.de>
2021-10-14 13:50:59 +0200

    Add Decode module with tests

---
+++ fsharp-intro/Intro.Test.Expecto/DecodeTest.fs
@@ -0,0 +1,156 @@
+﻿namespace Intro
+
+open Expecto
+
+module DecodeTest =
+  open Decode
+
+  [<Tests>]
+  let decodeStringTests =
+      testList "decodeString" [
+          test "fails when JSON is not a string" {
+              let input = Json.Boolean true
+              let actual = runDecoder decodeString input
+              Expect.isError actual "decoded boolean as string"
+          }
+
+          test "correctly decodes string values" {
+              let input = Json.String "abc foo"
+              let actual = runDecoder decodeString input
+              Expect.equal (Ok "abc foo") actual "decoded "
+          }
+      ]
+
+  // analog für decodeInt, decodeBool, decodeNull, ...
+
+  [<Tests>]
+  let decodeSpecificStringTests = testList "decodeSpecificString" [
+      test "works with the correct JSON string" {
+          let result = runDecoder (decodeSpecificString "foobar" 3) (Json.String "foobar")
+          Expect.equal result (Ok 3) "specific string failure"
+      }
+  ]
+
+  [<Tests>]
+  let decodeSuitTests = testList "decodeSuit" [
+      test "works for diamonds" {
+          let result = runDecoder DecodeCards.decodeSuit (Json.String "Kreuz")
+          Expect.equal result (Ok Cards.Clubs) ""
+      }
+  ]
+
+  [<Tests>]
+  let succeedTests =
+      // succeed should always succeed -> Property
+      testProperty "succeed always succeeds" (fun (json: Json.Value) value ->
+          runDecoder (succeed value) json = Ok value)
+
+  // konditionale Eigenschaften (braucht diesen Import)
+  open FsCheck
+
+  [<Tests>]
+  let decodeStringTests2 =
+      let isNiceString (s: string) = s <> null
+      testList "properties for decodeString" [
+          testProperty "succeed succeeds for nice strings" (fun (s: string) ->
+              isNiceString s ==> lazy (runDecoder decodeString (Json.String s) = Ok s))
+      ]
+
+  //Gen.sample 15 Arb.generate<list<int>>
+
+  // Arbitrary-Implementierung nötig, aber F# macht dies meistens automatisch via Reflection.
+  // Würden wir das händisch machen wollen/müssen (z.B. für rekursive Datentypen),
+  // dann benutzt man Generatoren (auch mit CEs):
+
+  type Foo =
+      | Bar of string * int
+      | Baz of list<bool>
+
+  open FsCheck
+
+  let fooGen =
+      // Beide Konstruktoren gleich häufig
+      Gen.oneof [
+          gen {
+              let! s = Arb.generate // Arb.generate<string>
+              let! i = Arb.generate
+              return Bar (s, i)
+          }
+          gen {
+              let! bools = Gen.listOf Arb.generate<bool>
+              return Baz bools
+          }
+      ]
+
+  // Beispiel: Häufigkeit der Konstruktoren steuern
+  let fooGenWithFreqs =
+      // Kürzere Schreibweise dank Kombinatoren wie bei Decode
+      Gen.frequency [
+          1, Gen.map2 (fun s i -> Bar (s, i)) Arb.generate Arb.generate
+          17, Gen.map Baz Arb.generate
+      ]
+
+  // Rekursive Typen sind problematisch, da hier Potenzial für
+  // StackOverflow besteht -> Size-Mechanismus manuell bedienen, um bei
+  // höherer Tiefe weniger zu generieren:
+
+  type MyRec = Bottom | Node of int * MyRec
+
+  let myRecGen =
+      // Higher-Order-Generator
+      let rec myRecSized size =
+          match size with
+          | 0 -> Gen.constant Bottom
+          | n when n > 0 ->
+              // smallerMyRec ist wieder ein Generator, aber "kleiner"
+              let smallerMyRec = myRecSized (size / 2)
+              Gen.oneof [ Gen.constant Bottom
+                          Gen.map2 (fun i mr -> Node (i, mr)) Arb.generate Arb.generate
+              ]
+          | _ -> failwith "Nur positive sizes erlaubt"
+      Gen.sized myRecSized
+
+  // Aber! F# muss diese selbstgeschriebenen Generatoren erst kennenlernen:
+
+  type MyGenerators () =
+      static member Foo () =
+          //{ new Arbitrary<Foo> () with
+          //    override _.Generator = fooGenWithFreqs
+          //    override _.Shrinker t = Seq.empty }
+          Arb.fromGen fooGenWithFreqs
+
+      static member MyRec () =
+          Arb.fromGen myRecGen
+
+  // Registriere die Generatoren global. FsCheck schaut in der Liste der
+  // Registrierungen nach, ob etwas existiert oder ob Reflection benutzt werden muss.
+  // Liste: Arb.Default
+  // Zu bestimmtem Typ "nachschauen": Arb.from<'a>
+  Arb.register<MyGenerators> () |> ignore
+
+  [<Tests>]
+  let decodeListTests = testList "decodeList" [
+      test "works for the empty list" {
+          let result = runDecoder (decodeList decodeInt) (Json.Array [])
+          Expect.equal result (Ok []) ""
+      }
+      test "works for two simple strings" {
+          let result =
+              [Json.String "a"; Json.String "b"]
+              |> Json.Array
+              |> runDecoder (decodeList decodeString)
+          Expect.equal result (Ok ["a"; "b"]) ""
+      }
+  ]
+
+  // Property-Test, der prüft, ob (im Wesentlichen) gilt:
+  // Dekodieren nach Kodieren == Identität
+  // (für CardsPlayed)
+  [<Tests>]
+  let decodeCardsPlayedTests =
+      testProperty "decode after encode is id" (fun cardsPlayed ->
+          let result =
+              cardsPlayed
+              |> Cards.encodeCardsPlayed
+              |> runDecoder DecodeCards.decodeCardsPlayed
+          result = Ok cardsPlayed)
---
+++ fsharp-intro/Intro/Decode.fs
@@ -0,0 +1,271 @@
+﻿namespace Intro
+
+module Decode =
+
+  type DecodeError =
+    // Dekodieren ist fehlgeschlagen
+    | Failure of string * Json.Value
+    | Field of string * DecodeError
+    | OneOf of list<DecodeError>
+
+  // type Either<'a, 'b> = Left of 'a | Right of 'b
+  // F# hat Result
+
+  // Ein Dekodierer dekodiert zu einem Wert vom Typ 'a.
+  // Dies kann fehlschlagen, daher verwenden wir Either.
+  type Decoder<'a> = Decoder of (Json.Value -> Result<'a, DecodeError>)
+
+  let runDecoder (Decoder decode) json =
+    decode json
+
+  let decodeString: Decoder<string> = Decoder (fun json ->
+    match json with
+    | Json.String s -> Ok s
+    | _ -> Failure ("Not a string", json) |> Error)
+
+  let decodeInt: Decoder<int> = Decoder (fun json ->
+    match json with
+    | Json.Number i -> Ok i // Achtung! Hier muesste normalerweise ein Check hin, ob i "wirklich" int ist
+    | _ -> Failure ("Not an integer", json) |> Error)
+
+  let decodeBool: Decoder<bool> = Decoder (fun json ->
+    match json with
+    | Json.Boolean b -> Ok b
+    | _ -> Failure ("Not a Boolean", json) |> Error)
+
+  // Kombinatoren: "Higher-Order-Dekodierer":
+  // Baue Dekodierer aus anderen Dekodierern/Bausteinen
+
+  // DecodeCards: hier decodeRank und decodeSuit schreiben
+
+  // Der erste wirkliche Kombinator: gegeben ein Wert resultVal,
+  // liefere einen Dekodierer, der bei Null den Wert zurueckgibt
+  let decodeNull (resultVal: 'a): Decoder<'a> = Decoder (fun json ->
+    match json with
+    | Json.Null -> Ok resultVal
+    | _ -> Failure ("Not null", json) |> Error)
+
+  // Decoder, der stets das gegebene x dekodiert und den Input ignoriert.
+  // Vergleiche Implementierung von _.Return in Maybe!
+  let succeed (x: 'a): Decoder<'a> = Decoder (fun _ -> Ok x)
+
+  // Decoder, der immer fehlschlaegt
+  let fail (msg: string): Decoder<'a> =
+    Decoder (function | json -> Failure (msg, json) |> Error)
+
+  let decodeField (field: string) aDecoder: Decoder<'a> =
+    Decoder (fun json ->
+        match json with
+        | Json.Object m ->
+          match Map.tryFind field m with
+          | None -> Failure (sprintf "Field %s not found" field, json) |> Error
+          | Some innerJson ->
+            runDecoder aDecoder innerJson
+            // Falls das fehlschlaegt, wollen wir eine bessere Fehlermeldung zurueckgeben,
+            // auch um sie vom aeusseren Fehler unterscheiden zu koennen -> Tag mit Fieldname
+            |> Result.mapError (fun e -> Field (field, e))
+        | _ -> Failure ("Not a JSON object", json) |> Error)
+
+  // DecodeCards: hier decodeCard schreiben
+
+  // Exkurs Applicative mit Ziel, decodeList zu schreiben fuer CardsPlayed
+
+  // Wir koennen ueber einen Decoder mappen und damit den Rueckgabewert aendern.
+  // Bzw.: Wir liften eine Funktion 'a -> 'b "in" Decoder:
+  //   Decoder<'a> -> Decoder<'b>
+  let map (f: 'a -> 'b) (decoder: Decoder<'a>) = Decoder (fun json ->
+    match runDecoder decoder json with
+    | Ok res -> Ok (f res)
+    | Error err -> Error err)
+
+  // map : 'a -> 'b -> Decoder<'a> -> Decoder<'b>
+  // wollen: map2 : 'a -> 'b -> 'c -> Decoder<'a> -> Decoder<'b> -> Decoder<'c>
+  // apply : Decoder<'a -> 'b> -> Decoder<'a> -> Decoder<'b>
+  //               ^ Wo taucht so etwas "natuerlich" auf?
+
+  // NACH map2, map3, ...!
+  let apply (aToBDecoder: Decoder<'a -> 'b>) aDecoder =
+    Decoder (fun json ->
+      match runDecoder aDecoder json with
+      | Error e -> Error e
+      | Ok a ->
+        match runDecoder aToBDecoder json with
+        | Error e -> Error e
+        | Ok f -> f a |> Ok)
+
+  let map2 (f: 'a -> 'b -> 'c) (aDecoder: Decoder<'a>) (bDecoder: Decoder<'b>) =
+    // f : 'a -> ('b -> 'c)
+    // map f : Decoder<'a> -> Decoder<'b -> 'c>
+    // map f aDecoder : Decoder<'b -> 'c>
+    apply (map f aDecoder) bDecoder
+
+  // Was ist mit map3, map4 usw.?
+
+  let (<*>) = apply
+
+  let map3 f aDec bDec cDec =
+    map f aDec <*> bDec <*> cDec
+
+  let map4 f aDec bDec cDec dDec =
+    map f aDec <*> bDec <*> cDec <*> dDec
+
+  // Auch hier koennen wir eine Bind-Operation definieren, die einen
+  // Decoder nimmt und mit einer Funktion verkettet, die auf dem Ergebnis
+  // operiert und einen weiteren Decoder liefert:
+  let bind: Decoder<'a> -> ('a -> Decoder<'b>) -> Decoder<'b> =
+    fun (Decoder decodeA) f ->
+      Decoder (fun json ->
+        match decodeA json with
+        | Ok a ->
+          // a ist das 'a im Erfolgsfall
+          let (Decoder decodeB) = f a
+          decodeB json
+        | Error e -> Error e)
+
+  let applyResult (aToBResult: Result<'a -> 'b, 'error>) aResult =
+    //match aResult with
+    //| Error e -> Error e
+    //| Ok a ->
+    //    match aToBResult with
+    //    | Error e -> Error e
+    //    | Ok f -> Ok (f a)
+    match aToBResult with
+    | Ok f -> Result.map f aResult
+    | Error e -> Error e
+
+  let map2Result: ('a -> 'b -> 'c) -> Result<'a, 'error> -> Result<'b, 'error> -> Result<'c, 'error> =
+    fun f aResult bResult ->
+      applyResult (Result.map f aResult) bResult
+
+  let traverseResult: ('a -> Result<'b, 'error>) -> list<'a> -> Result<list<'b>, 'error> =
+    let cons x rest = x :: rest
+    fun f xs ->
+      List.foldBack
+        (fun a acc -> map2Result cons (f a) acc)
+        xs
+        (Ok [])
+
+  // Dekodiere eine Liste von 'as anhand eines Dekodierers fuer 'a
+  let decodeList (Decoder decodeItem): Decoder<list<'a>> =
+    Decoder (function
+      | Json.Array arr ->
+        // Wollen decodeItem ueber die Liste mappen. ABER: falscher Typ!
+        arr |> traverseResult decodeItem
+      | json -> Failure ("Not a JSON array", json) |> Error)
+
+  let oks (results: list<Result<'a, 'error>>) =
+    results
+    |> List.collect (function | Ok a -> [a] | Error _ -> [])
+
+  let errors (results: list<Result<'a, 'error>>) =
+    results
+    |> List.collect (function | Error e -> [e] | Ok _ -> [])
+
+  // "Einer wird schon passen"
+  // Laziness waere nett hier
+  let decodeOneOf (aDecoders: list<Decoder<'a>>) = Decoder (fun json ->
+    let results = aDecoders |> List.map (fun decoder -> runDecoder decoder json)
+    match oks results with
+    | [] -> OneOf (errors results) |> Error
+    | x::_ -> Ok x)
+
+  let decodeOptional (aDecoder: Decoder<'a>): Decoder<option<'a>> =
+    decodeOneOf [map Some aDecoder; succeed None]
+
+  type DecoderBuilder () =
+    // Quasi map
+    member _.BindReturn (aDecoder, f) = map f aDecoder
+    //member _.ReturnFrom x = x
+    // benoetigt fuer and!
+    // Idee: fuehre beide Dekodierer "parallel" aus, sie sind gleichwertig,
+    // dann fasse Ergebnisse in Tupel zusammen
+    member _.MergeSources (aDecoder, bDecoder) =
+      //Decoder (fun json ->
+      //    let aRes = runDecoder aDecoder json
+      //    let bRes = runDecoder bDecoder json
+      //    match aRes, bRes with
+      //    | Ok a, Ok b -> Ok (a, b)
+      //    | Error e, Ok _ -> Error e
+      //    | Ok _, Error e -> Error e
+      //    | Error e1, Error e2 -> OneOf [e1; e2] |> Error)
+      // besser:
+      let toTuple a b = a, b
+      map2 toTuple aDecoder bDecoder
+    // Nur benötigt für decodeSpecificString
+    // let! monadisch (mehrere let!-Aufrufe nach and!, oder ohne and!)
+    member _.Bind (aDecoder, binder) = bind aDecoder binder
+    // return
+    member _.Return a = succeed a
+    // return!
+    member _.ReturnFrom aDecoder = aDecoder
+
+  let decoder = DecoderBuilder ()
+
+  let decodeSpecificString (s: string) (result: 'a) =
+    decoder {
+      let! str = decodeString
+      if str = s
+        then return result
+        else return! fail (sprintf "Expected %s" s)
+    }
+
+module DecodeCards =
+  open Cards
+  open Decode
+
+  let decodeSuit: Decoder<Cards.Suit> =
+    decodeOneOf [
+      decodeSpecificString "Karo" Cards.Diamonds
+      decodeSpecificString "Kreuz" Cards.Clubs
+      decodeSpecificString "Pik" Cards.Spades
+      decodeSpecificString "Herz" Cards.Hearts
+    ] 
+
+  // Alternativvorgehen:
+  let rankFromString: string -> Cards.Rank = function
+    | "7" -> Cards.Seven
+    | "8" -> Cards.Eight
+    | "9" -> Cards.Nine
+    | "10" -> Cards.Ten
+    | "Bube" -> Cards.Jack
+    | "Dame" -> Cards.Queen
+    | "König" -> Cards.King
+    | "Ass" -> Cards.Ace
+    // nicht schoen
+    | s -> failwithf "%s is not a valid value for Rank" s
+
+  let decodeRank: Decoder<Cards.Rank> =
+    map rankFromString decodeString
+
+  let decodeCard: Decoder<Cards.Card> =
+    decoder {
+      let! suit = decodeField "suit" decodeSuit
+      and! rank = decodeField "rank" decodeRank
+      return { Rank = rank; Suit = suit }
+    }
+
+  let decodeCardsPlayed: Decoder<Cards.CardsPlayed> =
+    decodeList decodeCard
+
+  // Mehrstellige Funktionen: applikatives Anwenden bzw. mapN
+  type CardRankCardsPlayed = CardRankCardsPlayed of Cards.Card * Cards.Rank * Cards.CardsPlayed
+
+  let curry3 f a b c = f (a, b, c)
+
+  let decodeCardRankCardsPlayed: Decoder<CardRankCardsPlayed> =
+    //map3 (curry3 CardRankCardsPlayed) decodeCard decodeRank decodeCardsPlayed
+    //decoder {
+    //    let! card = decodeCard
+    //    let! rank = decodeRank
+    //    let! cardsPlayed = decodeCardsPlayed
+    //    return CardRankCardsPlayed (card, rank, cardsPlayed)
+    //}
+    // Alternativ:
+    decoder {
+      let! card = decodeCard
+      // and! macht den applikativen Gedanken klar: die drei decodeX-Aufrufe
+      // sind unabhaengig voneinander
+      and! rank = decodeRank
+      and! cardsPlayed = decodeCardsPlayed
+      return CardRankCardsPlayed (card, rank, cardsPlayed)
+    }
