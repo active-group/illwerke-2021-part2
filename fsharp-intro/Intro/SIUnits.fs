8972442daf15c5a507a8aa1c601228fc33d33b26
Johannes Maier <johannes.maier@active-group.de>
2021-10-15 16:02:23 +0200

    Add very tiny Measure example

---
+++ fsharp-intro/Intro/SIUnits.fs
@@ -0,0 +1,18 @@
+﻿namespace Intro
+
+module SIUnits =
+  //open FSharp.Data.UnitSystems.SI.UnitSymbols
+  open FSharp.Data.UnitSystems.SI.UnitNames
+
+  let addOneMeter x = x + 1<meter>
+
+  type [<Measure>] foot
+
+  let footPerMeter: float<foot/meter> = 0.3048<_>
+
+  let meterToFoot (m: float<meter>): float<foot> =
+    footPerMeter * m
+
+  // Kommentare:
+  // - werden zur Kompilierzeit entfernet; zur Laufzeit kein Zugriff auf
+  //   die Maßeinheit möglich
